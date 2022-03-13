d0a17984c9c84d33fd1013e3872bc8dc55f64a84
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2022-03-13 14:58:12 +0100

    Directory changed for all files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import CounterHooks from './CounterHooks';
+import Toggler from './Toggler';
+
+function App() {
+	return (
+		<div className="App">
+			<CounterHooks />
+			<Toggler />
+		</div>
+	);
+}
+
+export default App;
+
+// ertaga git log dagi eski hashtagdan latest hashtag ga otib commit qilishni sorayman
+// xato init qilib push qilgandan keyin, dir ni ozgartirib commit ni boshqattan qilish kerak!
+// keyboard da qanday qilib close taglarni ohiriga ob otish
---
+++ src/hooks/useToggle.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+function useToggle(initial = false) {
+	const [ state, setState ] = useState(initial);
+	const toggle = () => {
+		setState(!state);
+	};
+	return [ state, toggle ];
+}
+export default useToggle;
